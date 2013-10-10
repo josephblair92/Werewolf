46a5070e4a6fbaccde537b1f33d0979edb80f9a2
Joseph <josephblair92@gmail.com>
2013-10-09 22:47:38 -0400

    October 9, 2013 - Changes to pom.xml, should be ready to deploy

--- src/main/java/edu/wm/werewolf/HomeController.java
+++ src/main/java/edu/wm/werewolf/HomeController.java
@@ -49,7 +49,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
